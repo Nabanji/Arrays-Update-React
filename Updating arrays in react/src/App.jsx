d9f1be8ceb37b4e609ba2a276e9dd6e576a39b7f
Prince Nabanji <pnabanji@gmail.com>
2024-03-28 12:57:28 +0300

    Functionality

---
+++ Updating arrays in react/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [foods, setFoods] = useState(["Apple", "Banana", "Orange"]);
+
+  function handleAddFood() {
+    const newFood = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value = "";
+
+    setFoods(f => [...f, newFood]);
+  }
+
+  function handleRemoveFood(index) {
+    setFoods(foods.filter((_, i) => i !== index))
+  }
+
+  return (
+    <>
+      <h1>List of food</h1>
+      <ul>
+        {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+      </ul>
+
+      <input
+        type="text"
+        id="foodInput"
+        placeholder='Enter food name'
+      />
+
+      <button onClick={handleAddFood}>Add food</button>
+    </>
+  )
+}
+
+export default App
